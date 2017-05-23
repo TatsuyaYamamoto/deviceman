41edda6415facba6330df61f71718920c0b427f7
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-05-23 12:27:02 +0900

    init with SPRING INITIALIZR

---
+++ src/main/java/jp/co/fujixerox/deviceman/DevicemanApplication.java
@@ -0,0 +1,12 @@
+package jp.co.fujixerox.deviceman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevicemanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevicemanApplication.class, args);
+	}
+}
