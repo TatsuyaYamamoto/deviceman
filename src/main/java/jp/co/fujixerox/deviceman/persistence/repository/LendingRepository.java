b6409add727bbcb5b23963c186f4fd71ecd45dbc
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-05-27 22:27:17 +0900

    fix, can't load active lending from DB.

--- src/main/java/jp/co/fujixerox/deviceman/persistence/repository/LendingRepository.java
+++ src/main/java/jp/co/fujixerox/deviceman/persistence/repository/LendingRepository.java
@@ -4,16 +4,17 @@ import jp.co.fujixerox.deviceman.persistence.entity.LendingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LendingRepository extends JpaRepository<LendingEntity, Integer>, JpaSpecificationExecutor<LendingEntity> {
     /**
-     * Find with {@link LendingEntity#deviceId}.
+     * Find active, someone is lending, Lending entity with {@link LendingEntity#deviceId}.
      *
      * @param deviceId
      * @return
      */
-    @Query("SELECT l FROM LendingEntity l WHERE l.actualReturnDate IS NOT NULL")
-    LendingEntity findActiveLendingByDeviceId(Integer deviceId);
+    @Query("SELECT l FROM LendingEntity l WHERE l.deviceId = :deviceId AND l.actualReturnDate IS NULL")
+    LendingEntity findActiveLendingByDeviceId(@Param("deviceId") Integer deviceId);
 }
